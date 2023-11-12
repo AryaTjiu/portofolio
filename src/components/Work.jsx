36348778cd8332cf3eaff7a76061e9dfd97b3b24
Arya Tjiutanto <aryatjiu.dev@gmail.com>
2023-11-12 14:47:04 +0700

    modify work parts

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,8 +7,8 @@ const Work = () => {
   const project = dataWork;
   
   return (
-    <div name='work' className='px-10 md:px-36 w-full min-h-screen md:h-screen py-36 xl:py-0 text-gray-300 bg-[#181818]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div name='work' className='px-10 md:px-36 xl:px-80 w-full min-h-screen py-36 text-gray-300 bg-[#181818]'>
+      <div className='mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline text-gray-300'>
             Work
@@ -16,32 +16,40 @@ const Work = () => {
           <p className='py-4'>// Check out some of my recent work</p>
         </div>
         {/* container for projects */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-12">
           {/* Gird Item */}
           {project.map((item, index) => (
-            <div key={index} style={{ backgroundImage: `url(assets/img/${item.image})` }} className="shadow-lg shadow-[#040c16] group container rounded-md  flex justify-center text-center items-center mx-auto content-div bg-cover z-0" data-aos="fade-up" data-aos-delay={index * 50}>
-              {/* Hover effect for images */}
-              <div className="opacity-0 group-hover:opacity-100 duration-500">
-                <div className='flex flex-col'>  
-                  <span className="text-2xl font bold text-white tracking-wider ">
-                    {item.name} 
-                  </span>
-                  {item.exercise &&
-                    <span>
-                      Exercise
-                    </span>
-                  }
+            <div className='flex flex-col space-y-7' id={item.id}>
+              <img src={`/assets/img/${item.image}`} className='w-full h-52 md:h-64'/>
+              <div className='flex flex-col space-y-4'>
+                <div className='flex justify-between'>
+                  <div className='grid'>  
+                    <span className='text-xl font-bold'>{item.name}</span>
+                    <span>{item.date}</span>
+                  </div>
                   <span>
-                    {item.date}
+                    {item.exercise? "exercise" : item.projectNote}
                   </span>
                 </div>
-                <div className="pt-8 text-center ">
+                <div className='flex items-center space-x-4'>
+                  <span>
+                    Tech: 
+                  </span>
+                  <div className='flex space-x-2'>
+                    {item.tech.map(data => (
+                        <span style={{ color : data[1] }}>
+                          {data[0]}
+                        </span>
+                    ))}
+                  </div>
+                </div>
+                <div className="flex space-x-4">
                   {/* eslint-disable-next-line */}
                   {
                     item.github &&
                     <a href={item.github} target="_blank">
                       <button
-                        className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg border-2 border-white hover:bg-transparent hover:text-white duration-200">
+                        className="text-center px-10 py-3 bg-white text-gray-700 border-2 border-white hover:bg-transparent hover:text-white duration-200">
                         Code
                       </button>
                     </a>
@@ -49,7 +57,7 @@ const Work = () => {
                   {/* eslint-disable-next-line */}
                   <a href={item.live} target="_blank">
                     <button
-                      className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg border-2 border-white hover:bg-transparent hover:text-white duration-200">
+                      className="text-center px-10 py-3 bg-transparent text-white border-2 border-white hover:bg-white hover:text-black duration-200">
                       Live
                     </button>
                   </a>
--- src/data/data.js
+++ src/data/data.js
@@ -6,45 +6,55 @@ export const dataWork=[
         id:1,
         name:"Lofipharse",
         exercise: true,
+        projectNote: "",
         date: "3/9/23",
         image:"lofipharse.jpg",
         github: null,
         live:"https://lofi-pharse.vercel.app/",
+        tech: [["react js", "#2cc0e6"]]
     },
     {
         id:2,
         name:"TourEase",
         exercise: true,
+        projectNote: "",
         date: "3/10/23",
         image:"tourease.jpg",
         github: null,
         live:"https://tour-ease-5j6rmf45f-syegi.vercel.app/",
+        tech: [["react js", "#2cc0e6"]]
     },
     {
         id:3,
         name:"CineHunt",
         exercise: true,
+        projectNote: "",
         date: "11/10/23",
         image:"cinehunt.jpg",
         github:null,
         live:"https://cinehunt.vercel.app/",
+        tech: [["react js", "#2cc0e6"]]
     },
     {
         id:4,
         name:"4clean",
         exercise: false,
+        projectNote: "Practical assignments",
         date: "8/9/22",
         image:"4clean.jpg",
         github:"https://github.com/4clean/web",
         live:"https://4clean.github.io/web/",
+        tech: [["HTML", "#d16908"], ["CSS", "#00c3d9"], ["Javascript", "#d6b709"]]
     },
     // {
     //     id:4,
     //     name:"Tokosl 88",
     //     exercise: false,
+    //     projectNote: "",
     //     date: "12/8/23",
     //     image:"tokosl88.jpg",
     //     live:"https://www.tokosl88.com/",
+    //     tech: [["react js", "#2cc0e6"]]
     // },
 ]
 
@@ -69,11 +79,6 @@ export const dataSkill = [
         name:"TailwindCSS",
         percentage:85
     },
-    {
-        id:5,
-        name:"Firestore (learning)",
-        percentage:15
-    }
 ]
 
 export const dataTimelines = [
